e5644114fa4d6eb6b9a45bdbd5bd8936f0ee032c
AndreyCorpse28090 <280990corpse@gmail.com>
2024-07-06 21:02:06 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace ProcessViewer
+{
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer _timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadProcesses();
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(10); // Обновление каждые 10 секунд
+            _timer.Tick += (s, e) => LoadProcesses();
+            _timer.Start();
+        }
+
+        private void LoadProcesses()
+        {
+            var processes = Process.GetProcesses()
+                .Select(p => new
+                {
+                    p.ProcessName,
+                    p.Id,
+                    MemoryUsage = $"{p.WorkingSet64 / 1024 / 1024} MB"
+                }).ToList();
+            ProcessListView.ItemsSource = processes;
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadProcesses();
+        }
+
+        private void ProcessListView_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (ProcessListView.SelectedItem != null)
+            {
+                var selectedProcess = (dynamic)ProcessListView.SelectedItem;
+                var processId = selectedProcess.Id;
+                var detailsWindow = new ProcessDetailsWindow(processId);
+                detailsWindow.Show();
+            }
+        }
+    }
+}
---
+++ ProcessDetailsWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace ProcessViewer
+{
+    public partial class ProcessDetailsWindow : Window
+    {
+        private int _processId;
+
+        public ProcessDetailsWindow(int processId)
+        {
+            InitializeComponent();
+            _processId = processId;
+            LoadProcessDetails();
+        }
+
+        private void LoadProcessDetails()
+        {
+            try
+            {
+                var process = Process.GetProcessById(_processId);
+                ProcessDetailsTextBlock.Text = $"Name: {process.ProcessName}\n" +
+                                               $"ID: {process.Id}\n" +
+                                               $"Start Time: {process.StartTime}\n" +
+                                               $"Total Processor Time: {process.TotalProcessorTime}\n" +
+                                               $"Memory Usage: {process.WorkingSet64 / 1024 / 1024} MB\n" +
+                                               $"Threads: {process.Threads.Count}\n";
+            }
+            catch (Exception ex)
+            {
+                ProcessDetailsTextBlock.Text = $"Error: {ex.Message}";
+            }
+        }
+    }
+}
